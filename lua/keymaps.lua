200a5d46b6f5982cc733c4c0301de49326898d24
REEPSIW <mihailzomitev@gmail.com>
2022-02-10 20:42:37 +0300

    line moving added

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -9,4 +9,8 @@ end
 -- Bufferline.nvim
 map('n', '<Tab>', ':BufferLineCycleNext<CR>', { silent = true })
 map('n', '<S-Tab>', ':BufferLineCyclePrev<CR>', { silent = true })
-
+-- Moving lines and blocks
+map('n', '<C-Up>', ':MoveLine(-1)<CR>', {silent = true})
+map('n', '<C-Down>', ':MoveLine(1)<CR>', {silent = true})
+map('v', '<C-Up>', ':MoveBlock(-1)<CR>', {silent = true})
+map('v', '<C-Down>', ':MoveBlock(1)<CR>', {silent = true})
