0e7d6af3e0067a61153dc5d885d62718f6226df6
REEPSIW <mihailzomitev@gmail.com>
2022-02-11 22:41:31 +0300

    added LSP and some plugins

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -14,3 +14,7 @@ map('n', '<C-Up>', ':MoveLine(-1)<CR>', {silent = true})
 map('n', '<C-Down>', ':MoveLine(1)<CR>', {silent = true})
 map('v', '<C-Up>', ':MoveBlock(-1)<CR>', {silent = true})
 map('v', '<C-Down>', ':MoveBlock(1)<CR>', {silent = true})
+-- LSP saga renaming
+map('n', 'gr', ':Lspsaga rename<CR>', {silent = true})
+-- LSP saga preview definition
+map('n', 'gd', ':Lspsaga preview_definition<CR>', {silent = true})
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,19 +1,43 @@
 return require('packer').startup(function()
     -- Packer can manage itself
     use 'wbthomason/packer.nvim'
+    -- Start page
     use 'glepnir/dashboard-nvim'
+    -- Auto pairs
     use 'windwp/nvim-autopairs'
+    -- Rainbow brackets
     use 'luochen1990/rainbow'
+    -- Better comment
     use 'tomtom/tcomment_vim'
+    -- Float terminal
+    use 'jiajiawang/fterm.nvim'
     -- Move lines
     use 'fedepujol/move.nvim'
+    -- Highlight operators
+    use 'Valloric/vim-operator-highlight'
+    -- Folding
+    use 'pierreglaser/folding-nvim'
     -- Show css colors
     use 'norcalli/nvim-colorizer.lua'
     -- Better cpp highlight
     use 'octol/vim-cpp-enhanced-highlight'
     -- Nvim LSP
     use 'neovim/nvim-lspconfig'
-    -- Autocomplete
+    -- Renaming etc
+    use 'glepnir/lspsaga.nvim'
+    use 'williamboman/nvim-lsp-installer'
+        -- Autocomplete
+            -- Nvim-cmp
+            use 'hrsh7th/cmp-nvim-lsp'
+            use 'hrsh7th/cmp-buffer'
+            use 'hrsh7th/cmp-path'
+            use 'hrsh7th/cmp-cmdline'
+            use 'hrsh7th/nvim-cmp'
+            -- use 'hrsh7th/cmp-vsnip'
+            use 'saadparwaiz1/cmp_luasnip'
+        -- Snippets
+        use 'L3MON4D3/LuaSnip'
+        -- use 'hrsh7th/vim-vsnip'
     use {
     'nvim-lualine/lualine.nvim',
     requires = { 'kyazdani42/nvim-web-devicons', opt = true }
