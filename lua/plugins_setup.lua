052e3c6dd1bf5db595e757f4202bb727d96af13a
REEPSIW <mihailzomitev@gmail.com>
2022-02-11 22:40:29 +0300

    Plugins Initialization

---
+++ lua/plugins_setup.lua
@@ -0,0 +1,10 @@
+-- Little plugins settings
+require('plugins_settings')
+-- Bufferline nvim
+require('advanced_plugins/bufferline_settings')
+-- Lualine.nvim
+require('advanced_plugins/lualine_settings')
+-- Nvim lsp completion
+require('LSP_config/nvim-lspconfig')
+require('LSP_config/nvim-cmp')
+require('LSP_config.lspsaga-nvim')
