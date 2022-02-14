657b17e06d2a85592ea80a4c0434b832fc1e17a0
REEPSIW <mihailzomitev@gmail.com>
2022-02-14 20:55:20 +0300

    Some futures

--- lua/settings.lua
+++ lua/settings.lua
@@ -4,6 +4,8 @@ set.number = true
 -- Show spaces
 set.listchars:append({ space = '·', trail= '·'})
 set.list = true
+set.fileencoding='utf-8'
+set.encoding='utf-8'
 -- Indent settings
 set.expandtab = true
 set.smarttab = true
@@ -17,4 +19,16 @@ set.undofile = true
 set.ignorecase = true
 -- Enable mouse
 set.mouse = 'a'
+-- Show sign on the left side
 set.signcolumn = 'yes'
+-- Number of lines to keep above and below the cursor
+set.sidescrolloff = 8
+-- Don't wrap lines
+set.wrap = false
+-- For faster work
+set.cursorline = false
+set.cursorcolumn = false
+-- Better folding
+set.foldmethod='expr'
+set.foldexpr = 'nvim_treesitter#foldexpr()'
+set.foldlevel = 999
