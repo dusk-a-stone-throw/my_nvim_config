47564f2034d95ab35f2f72d5a8f8a4f45513ac8a
REEPSIW <mihailzomitev@gmail.com>
2022-02-16 15:23:03 +0300

    Minor command added

--- lua/commands.lua
+++ lua/commands.lua
@@ -2,5 +2,6 @@ local g = vim.g
 -- command FTermToggle lua require'fterm'.toggle()
 vim.cmd [[
     command FTermToggle lua require'fterm'.toggle() 
+    " I really need this
+    command Wq wq
 ]]
--- vim.g.nvim_add_user_command('FTermToggle',':lua require\'fterm\'.toggle()')
