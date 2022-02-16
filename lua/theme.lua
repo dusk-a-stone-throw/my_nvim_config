12c17af2f5824e6be65bdd24776ae0256733d1f0
REEPSIW <mihailzomitev@gmail.com>
2022-02-16 15:22:34 +0300

    better bg

--- lua/theme.lua
+++ lua/theme.lua
@@ -15,13 +15,16 @@ require('onedark').setup {
     Comment = {fg = '#48b84d', fmt = 'none'},
     PunctBracket = {fg = '#000000', bg = '#Ac3146'}
     },
-    toggle_style_key = '<leader>ts', -- Default keybinding to toggle
+    -- toggle_style_key = '<leader>ts', -- Default keybinding to toggle
     toggle_style_list = {'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light'},
 
 }
-
 -- Tokyo night
 g.tokyonight_style = "night"
 -- Setup
--- require('onedark').load()
+-- Better background color
+if vim.cmd[[ :silent colorscheme ]]=='nvcode' then
+    vim.cmd [[ hi Normal guifg=#abb2bf ctermfg=249 guibg=#121212 ctermbg=234 gui=NONE cterm=NONE ]]
+    vim.cmd [[ hi SignColumn guifg=NONE ctermfg=NONE guibg=#121212 ctermbg=234 gui=NONE cterm=NONE ]]
+end
 vim.cmd [[ colorscheme nvcode ]]
