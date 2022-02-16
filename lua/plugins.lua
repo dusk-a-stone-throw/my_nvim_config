4f6192cb12b6a39cde3d3ee1a0d3e04dade21323
REEPSIW <mihailzomitev@gmail.com>
2022-02-16 21:16:55 +0300

    Better organization

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,52 +1,84 @@
 return require('packer').startup(function()
-    -- Cheat sheet
-    use {
-    'sudormrfbin/cheatsheet.nvim',
-        requires = {
-            {'nvim-telescope/telescope.nvim'},
-            {'nvim-lua/popup.nvim'},
-            {'nvim-lua/plenary.nvim'},
-        }
-    }
-    -- Automatically creates missing dirs when saving a file
-    use 'jghauser/mkdir.nvim'
-    -- Git integration
-    use  'lewis6991/gitsigns.nvim'
-    -- Optimiser
+    -- Optimiser (should be on the top)
     use 'lewis6991/impatient.nvim'
-    -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
-    -- Start page
-    use 'glepnir/dashboard-nvim'
-    -- Convert vim map to lua
-    use 'thugcee/nvim-map-to-lua'
-    -- Multiple cursors
-    use 'terryma/vim-multiple-cursors'
-    -- Auto pairs
-    use 'windwp/nvim-autopairs'
-    -- Get startup time
-    use 'dstein64/vim-startuptime'
-    -- Better comment
-    use 'tomtom/tcomment_vim'
-    -- Float terminal
-    use 'jiajiawang/fterm.nvim'
-    -- Move lines
-    use 'fedepujol/move.nvim'
-    -- Folding
-    use 'pierreglaser/folding-nvim'
-    -- Show css colors
-    use 'norcalli/nvim-colorizer.lua'
-    -- Highlight entered ranges
-    use 'winston0410/range-highlight.nvim'
-    -- Awesome highlighting
-    use {
-        'nvim-treesitter/nvim-treesitter',
-        run = ':TSUpdate'
-    }
-    -- Code runner
-    -- Rainbow brackets
-    use 'p00f/nvim-ts-rainbow'
-    -- Nvim LSP
+
+
+
+-- ======================== Utility ========================
+        -- Formatting
+        use 'mhartington/formatter.nvim'
+        -- Automatically creates missing dirs when saving a file
+        use 'jghauser/mkdir.nvim'
+        -- Git integration
+        use  'lewis6991/gitsigns.nvim'
+        -- Packer can manage itself
+        use 'wbthomason/packer.nvim'
+        -- Start page
+        use 'glepnir/dashboard-nvim'
+        -- Convert vim map to lua
+        use 'thugcee/nvim-map-to-lua'
+        -- Multiple cursors
+        use 'terryma/vim-multiple-cursors'
+        -- Auto pairs
+        use 'windwp/nvim-autopairs'
+        -- Get startup time
+        use 'dstein64/vim-startuptime'
+        -- Fast comment
+        use 'tomtom/tcomment_vim'
+        -- Float terminal
+        use 'jiajiawang/fterm.nvim'
+        -- Move lines
+        use 'fedepujol/move.nvim'
+        -- Folding
+        use 'pierreglaser/folding-nvim'
+        -- Searcher
+        use {
+            'nvim-telescope/telescope.nvim',
+            requires = { {'nvim-lua/plenary.nvim'} }
+        }
+
+
+
+-- ======================== Appearance ========================
+        -- Show css colors
+        use 'norcalli/nvim-colorizer.lua'
+
+        -- Highlight entered ranges
+        use 'winston0410/range-highlight.nvim'
+
+        -- Awesome highlighting
+        use {
+            'nvim-treesitter/nvim-treesitter',
+            run = ':TSUpdate'
+        }
+
+        -- Rainbow brackets
+        use 'p00f/nvim-ts-rainbow'
+        -- Awesome statusline
+
+        use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+        }
+
+        -- Awesome bufferline
+        use 'akinsho/bufferline.nvim'
+
+        -- Themes
+        use 'navarasu/onedark.nvim'
+        use 'folke/tokyonight.nvim'
+        use 'marko-cerovac/material.nvim'
+        use 'ChristianChiarulli/nvcode-color-schemes.vim'
+        use 'rafamadriz/neon'
+
+        -- Theme switcher
+        use 'xolox/vim-colorscheme-switcher'
+            -- Required
+            use 'xolox/vim-misc'
+
+
+
+-- ======================== Nvim LSP ======================== 
     use 'neovim/nvim-lspconfig'
     -- Renaming etc
     use 'glepnir/lspsaga.nvim'
@@ -62,26 +94,13 @@ return require('packer').startup(function()
             use 'saadparwaiz1/cmp_luasnip'
         -- Snippets
         use 'L3MON4D3/LuaSnip'
-        -- use 'hrsh7th/vim-vsnip'
-    use {
-    'nvim-lualine/lualine.nvim',
-    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-    }
-    use 'akinsho/bufferline.nvim'
-    use {
-        'nvim-telescope/telescope.nvim',
-        requires = { {'nvim-lua/plenary.nvim'} }
-    }
-    -- Formatting
-    use 'mhartington/formatter.nvim'
-    -- Theme switcher
-    use 'xolox/vim-colorscheme-switcher'
-        -- Required
-        use 'xolox/vim-misc'
-    -- Themes
-    use 'navarasu/onedark.nvim'
-    use 'folke/tokyonight.nvim'
-    use 'marko-cerovac/material.nvim'
-    use 'ChristianChiarulli/nvcode-color-schemes.vim'
-    use 'rafamadriz/neon'
+
+
+
+-- ======================== Other stuff ======================== 
+        -- Cheat sheet
+        use 'sudormrfbin/cheatsheet.nvim'
+            -- Required
+            use 'nvim-lua/popup.nvim'
+
 end)
