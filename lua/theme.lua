196ac5b77a167e5d24f574574538f89f402d3c18
REEPSIW <mihailzomitev@gmail.com>
2022-12-06 15:46:38 +0300

    new theme

--- lua/theme.lua
+++ lua/theme.lua
@@ -11,12 +11,12 @@ require('onedark').setup {
         variables = 'none'
     },
     highlights = {
-    String = {fg = '#de9623', fmt = 'none'},
-    Comment = {fg = '#48b84d', fmt = 'none'},
-    PunctBracket = {fg = '#000000', bg = '#Ac3146'}
+        String = { fg = '#de9623', fmt = 'none' },
+        Comment = { fg = '#48b84d', fmt = 'none' },
+        PunctBracket = { fg = '#000000', bg = '#Ac3146' }
     },
     -- toggle_style_key = '<leader>ts', -- Default keybinding to toggle
-    toggle_style_list = {'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light'},
+    toggle_style_list = { 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light' },
 
 }
 
@@ -25,17 +25,14 @@ g.tokyonight_style = 'night'
 
 -- Github-nvim-theme (light)
 require('github-theme').setup({
-  theme_style = 'light', -- dark/dimmed/dark_default/dark_colorblind/light/light_default/light_colorblind
-  function_style = 'NONE',
-  keyword_style = 'NONE',
-  sidebars = {'qf', 'vista_kind', 'terminal', 'packer'},
+    theme_style = 'dark', -- dark/dimmed/dark_default/dark_colorblind/light/light_default/light_colorblind
+    function_style = 'NONE',
+    keyword_style = 'NONE',
+    sidebars = { 'qf', 'vista_kind', 'terminal', 'packer' },
 })
 
 
 -- ========== Setup ==========
 -- Better background color
-if vim.cmd[[ :silent colorscheme ]]=='nvcode' then
-    vim.cmd [[ hi Normal guifg=#abb2bf ctermfg=249 guibg=#121212 ctermbg=234 gui=NONE cterm=NONE ]]
-    vim.cmd [[ hi SignColumn guifg=NONE ctermfg=NONE guibg=#121212 ctermbg=234 gui=NONE cterm=NONE ]]
-end
-vim.cmd [[ colorscheme nvcode ]]
+
+vim.cmd [[ colorscheme vscode]]
