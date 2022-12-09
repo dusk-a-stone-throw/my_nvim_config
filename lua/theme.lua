ac628bf160dfce2f24eb1e180e81a605cef47193
REEPSIW <mihailzomitev@gmail.com>
2022-12-09 14:45:05 +0300

    Tokyo night

--- lua/theme.lua
+++ lua/theme.lua
@@ -27,8 +27,25 @@ require('onedark').setup {
 
 
 -- Tokyo night
-g.tokyonight_style = 'night'
-
+require('tokyonight').setup({
+  -- your configuration comes here
+  -- or leave it empty to use the default settings
+  style = 'night', -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+  light_style = 'day', -- The theme is used when the background is set to light
+  transparent = false, -- Enable this to disable setting the background color
+  terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+  styles = {
+    -- Style to be applied to different syntax groups
+    -- Value is any valid attr-list value for `:help nvim_set_hl`
+    comments = { italic = false },
+    keywords = { italic = false },
+    functions = {},
+    variables = {},
+    -- Background styles. Can be 'dark', 'transparent' or 'normal'
+    sidebars = 'dark', -- style for sidebars, see below
+    floats = 'dark', -- style for floating windows
+  }
+})
 
 
 -- Github-nvim-theme (light)
@@ -55,4 +72,4 @@ require('vscode').setup({
 
 
 -- ========== Setup ==========
--- vim.cmd [[ colorscheme vscode]]
+vim.cmd [[ colorscheme tokyonight-night]]
