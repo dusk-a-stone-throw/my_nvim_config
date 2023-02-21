2c3ca4d63a3aa0e0c1b65fc4f6edb1f1914f2a72
REEPSIW <mihailzomitev@gmail.com>
2023-02-21 13:59:29 +0300

    Updated removed deprecated

--- lua/plugins_config/dashboard.lua
+++ lua/plugins_config/dashboard.lua
@@ -1,46 +1,57 @@
 local db = require('dashboard')
--- g.dashboard_default_executive = 'telescope'
-db.custom_header = {
-    '                                   ',
-    '                                   ',
-    '                                   ',
-    '   ⣴⣶⣤⡤⠦⣤⣀⣤⠆     ⣈⣭⣿⣶⣿⣦⣼⣆          ',
-    '    ⠉⠻⢿⣿⠿⣿⣿⣶⣦⠤⠄⡠⢾⣿⣿⡿⠋⠉⠉⠻⣿⣿⡛⣦       ',
-    '          ⠈⢿⣿⣟⠦ ⣾⣿⣿⣷    ⠻⠿⢿⣿⣧⣄     ',
-    '           ⣸⣿⣿⢧ ⢻⠻⣿⣿⣷⣄⣀⠄⠢⣀⡀⠈⠙⠿⠄    ',
-    '          ⢠⣿⣿⣿⠈    ⣻⣿⣿⣿⣿⣿⣿⣿⣛⣳⣤⣀⣀   ',
-    '   ⢠⣧⣶⣥⡤⢄ ⣸⣿⣿⠘  ⢀⣴⣿⣿⡿⠛⣿⣿⣧⠈⢿⠿⠟⠛⠻⠿⠄  ',
-    '  ⣰⣿⣿⠛⠻⣿⣿⡦⢹⣿⣷   ⢊⣿⣿⡏  ⢸⣿⣿⡇ ⢀⣠⣄⣾⠄   ',
-    ' ⣠⣿⠿⠛ ⢀⣿⣿⣷⠘⢿⣿⣦⡀ ⢸⢿⣿⣿⣄ ⣸⣿⣿⡇⣪⣿⡿⠿⣿⣷⡄  ',
-    ' ⠙⠃   ⣼⣿⡟  ⠈⠻⣿⣿⣦⣌⡇⠻⣿⣿⣷⣿⣿⣿ ⣿⣿⡇ ⠛⠻⢷⣄ ',
-    '      ⢻⣿⣿⣄   ⠈⠻⣿⣿⣿⣷⣿⣿⣿⣿⣿⡟ ⠫⢿⣿⡆     ',
-    '       ⠻⣿⣿⣿⣿⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⡟⢀⣀⣤⣾⡿⠃     ',
-    '         ⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋       ',
-}
+db.setup({
+    theme = 'doom',
+    config = {
+        header = {
+            '                                   ',
+            '                                   ',
+            '                                   ',
+            '   ⣴⣶⣤⡤⠦⣤⣀⣤⠆     ⣈⣭⣿⣶⣿⣦⣼⣆          ',
+            '    ⠉⠻⢿⣿⠿⣿⣿⣶⣦⠤⠄⡠⢾⣿⣿⡿⠋⠉⠉⠻⣿⣿⡛⣦       ',
+            '          ⠈⢿⣿⣟⠦ ⣾⣿⣿⣷    ⠻⠿⢿⣿⣧⣄     ',
+            '           ⣸⣿⣿⢧ ⢻⠻⣿⣿⣷⣄⣀⠄⠢⣀⡀⠈⠙⠿⠄    ',
+            '          ⢠⣿⣿⣿⠈    ⣻⣿⣿⣿⣿⣿⣿⣿⣛⣳⣤⣀⣀   ',
+            '   ⢠⣧⣶⣥⡤⢄ ⣸⣿⣿⠘  ⢀⣴⣿⣿⡿⠛⣿⣿⣧⠈⢿⠿⠟⠛⠻⠿⠄  ',
+            '  ⣰⣿⣿⠛⠻⣿⣿⡦⢹⣿⣷   ⢊⣿⣿⡏  ⢸⣿⣿⡇ ⢀⣠⣄⣾⠄   ',
+            ' ⣠⣿⠿⠛ ⢀⣿⣿⣷⠘⢿⣿⣦⡀ ⢸⢿⣿⣿⣄ ⣸⣿⣿⡇⣪⣿⡿⠿⣿⣷⡄  ',
+            ' ⠙⠃   ⣼⣿⡟  ⠈⠻⣿⣿⣦⣌⡇⠻⣿⣿⣷⣿⣿⣿ ⣿⣿⡇ ⠛⠻⢷⣄ ',
+            '      ⢻⣿⣿⣄   ⠈⠻⣿⣿⣿⣷⣿⣿⣿⣿⣿⡟ ⠫⢿⣿⡆     ',
+            '       ⠻⣿⣿⣿⣿⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⡟⢀⣀⣤⣾⡿⠃     ',
+            '         ⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋       ',
+        },
+        packages = {
+            enable = false,
+        },
+        week_header = {
+            enable = false,
+        },
+        center = {
+            { icon = '  ',
+                desc = 'New file                                ',
+                action = 'enew',
+            },
+            { icon = '  ',
+                desc = 'Recently opened files                   ',
+                action = 'Telescope oldfiles',
+            },
+            { icon = '  ',
+                desc = 'Find  File                              ',
+                action = 'Telescope find_files find_command=rg,--hidden,--files',
+            },
+            { icon = '  ',
+                desc = 'Find  word                              ',
+                action = 'Telescope live_grep',
+            },
+            { icon = '  ',
+                desc = 'Change colorscheme                      ',
+                action = 'Telescope colorscheme',
+            },
+            { icon = 'ﰸ  ',
+                desc = 'Exit                                    ',
+                action = 'q',
+            },
 
-db.custom_center = {
-    { icon = '  ',
-        desc = 'New file                                ',
-        action = 'DashboardNewFile',
-        shortcut = 'SPC c n' },
-    { icon = '  ',
-        desc = 'Recently opened files                   ',
-        action = 'Telescope oldfiles',
-        shortcut = 'SPC f h' },
-    { icon = '  ',
-        desc = 'Find  File                              ',
-        action = 'Telescope find_files find_command=rg,--hidden,--files',
-        shortcut = 'SPC f f' },
-    { icon = '  ',
-        desc = 'Find  word                              ',
-        action = 'Telescope live_grep',
-        shortcut = 'SPC f w' },
-    { icon ='  ',
-        desc = 'Change colorscheme                      ',
-        action = 'Telescope colorscheme',
-        shortcut = 'SPC t c' },
-
-    }
-db.custom_footer = {
-    'By Vargoshi'
-}
+        },
+        footer = { 'By Vargoshi' }
+    },
+})
