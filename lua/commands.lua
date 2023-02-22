db7fa56e7e38c99f66d76413b41237db992495a3
REEPSIW <mihailzomitev@gmail.com>
2023-02-22 19:23:34 +0300

    BETA Word mode

--- lua/commands.lua
+++ lua/commands.lua
@@ -4,4 +4,8 @@ vim.cmd [[
     " I really need this
     command Wq wq
     command Wqa wqa
+    " 'Word office' mode
+    command WordmodeEnable colorscheme tokyonight-day | highlight Normal guifg=#000000 guibg=#FFFFFF | set nolist | set nonumber | set signcolumn=no | TSDisable highlight | TSDisable rainbow | set syntax=off | set wrap | set laststatus=0 | set noshowmode | set noshowcmd | set showtabline=0 | LspStop
+    command WordmodeDisable colorscheme tokyonight | set list | set number | set signcolumn=yes | TSEnable highlight | TSEnable rainbow | set syntax=on | set nowrap | set laststatus=2  | LspStart
 ]]
+
