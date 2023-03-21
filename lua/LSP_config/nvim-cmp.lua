6f236b1b39260fc754374c9ade0527d9a8d77194
REEPSIW <mihailzomitev@gmail.com>
2023-03-21 20:55:20 +0300

    need to test

--- lua/LSP_config/nvim-cmp.lua
+++ lua/LSP_config/nvim-cmp.lua
@@ -123,9 +123,7 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protoc
 require('lspconfig')['lua_ls'].setup({
     capabilities = capabilities,
 })
-require('lspconfig')['pyright'].setup({
-    capabilities = capabilities,
-})
+require('lspconfig').pylsp.setup({})
 require('lspconfig')['clangd'].setup({
     capabilities = capabilities,
 })
