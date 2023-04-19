a1a55a9647216ce7f94ad205906a92d9df710a27
REEPSIW <mihailzomitev@gmail.com>
2023-04-19 19:02:57 +0300

    added parser for vimdoc

--- lua/plugins_config/treesitter.lua
+++ lua/plugins_config/treesitter.lua
@@ -1,6 +1,6 @@
 require('nvim-treesitter.configs').setup({
     -- One of "all", "maintained" (parsers with maintainers), or a list of languages
-    ensure_installed = { 'cpp', 'python', 'lua' },
+    ensure_installed = { 'cpp', 'python', 'lua', 'vimdoc' },
     highlight = {
         enable = true,
     },
