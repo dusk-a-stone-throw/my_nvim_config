9b511f0a2c9ec144b9c0cd004c65be12891b17b3
Mikhail <mihailzomitev@gmail.com>
2026-04-03 22:38:17 +0300

    autoinstall for c/cpp, lua, python, go, bash, vim/vimdoc, markdown

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -1,1 +1,27 @@
 require('nvim-treesitter').setup({})
+require('nvim-treesitter').install({
+    -- LUA
+    'lua',
+    'luadoc',
+
+    -- C / C++
+    'c',
+    'cpp',
+
+    -- PYTHON
+    'python',
+
+    -- GO
+    'go',
+
+    -- BASH
+    'bash',
+
+    -- VIM
+    'vim',
+    'vimdoc',
+
+    -- MARKDOWN
+    'markdown',
+    'markdown_inline',
+})
